63bb3e4f0913b52bc253e59fbc63c61af3873700
SniperPRX <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-03-18 17:53:52 +0000

    Update PbA.ml

--- ProblemaA/PbA.ml
+++ ProblemaA/PbA.ml
@@ -1,1 +1,17 @@
-(*ola*)
+open F_parser
+
+type formula =
+| Var of variable
+| Not of formula
+| And of formula * formula
+| Or of formula * formula
+| Implies of formula * formula
+| Equiv of formula * formula
+| True
+| False
+
+val parse: string -> formula list option
+
+
+
+(*ocamlc -I f_parser/ f_parser.cma PbA.ml -o PbA*)
