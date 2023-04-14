81a3a1ed480dd6f900d824a1e4016d403c5bc251
SniperPRX <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-04-14 12:11:51 +0100

    Update problemaA.ml

--- ProblemaA/problemaA.ml
+++ ProblemaA/problemaA.ml
@@ -12,9 +12,12 @@ type formula =
   | Or of formula * formula
   | Implies of formula * formula
   | Equiv of formula * formula
-  | Nor of formula * formula
   | True
   | False
+
+type nor_formula =
+  | Var of variable
+  | Nor of formula * formula
 
 let rec to_nor = function
   | Nor (a, b) -> Nor (to_nor a, to_nor b)
@@ -22,19 +25,21 @@ let rec to_nor = function
   | Not f -> Nor(to_nor f, to_nor f)
   | And (a, b) -> Nor(Nor(to_nor a, to_nor a), Nor(to_nor b, to_nor b))
   | Or (a, b) -> Nor(Nor(to_nor a, to_nor b), Nor(to_nor a, to_nor b))
-  | Implies (a, b) -> to_nor Not(Nor((Not a), b))
-  | Equiv (a, b) -> to_nor And(Implies(a, b), Implies(b, a))
+  | Implies (a, b) -> to_nor (Not (Nor((Not a), b)))
+  | Equiv (a, b) -> to_nor (And(Implies(a, b), Implies(b, a)))
+  | False f -> Nor(to_nor f, (Nor(to_nor f, to_nor f)))
+  | True f -> to_nor (Not(False f))
 
 let rec formula_to_string (f : formula) : string =
   match f with
-  | Nor (f1, f2) -> Printf.sprintf "(%s '%' %s)" (formula_to_string f1) (formula_to_string f2)
+  | Nor (f1, f2) -> Printf.sprintf "(%s %% %s)" (formula_to_string f1) (formula_to_string f2)
   | And (f1, f2) -> Printf.sprintf "(%s & %s)" (formula_to_string f1) (formula_to_string f2)
   | Or (f1, f2) -> Printf.sprintf "(%s | %s)" (formula_to_string f1) (formula_to_string f2)
   | Not f1 -> Printf.sprintf "!%s" (formula_to_string f1)
-  | Imply (f1, f2) -> Printf.sprintf "(%s -> %s)" (formula_to_string f1) (formula_to_string f2)
+  | Implies (f1, f2) -> Printf.sprintf "(%s -> %s)" (formula_to_string f1) (formula_to_string f2)
   | Equiv (f1, f2) -> Printf.sprintf "(%s <-> %s)" (formula_to_string f1) (formula_to_string f2)
-  | True -> "TRUE"
-  | False -> "FALSE"
+  | True f1 -> "TRUE"
+  | False f1 -> "FALSE"
 
 let f_list = parse "stdin"
 
