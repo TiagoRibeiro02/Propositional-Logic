e18e8b91b003b56764c28c08106222b7cb29e3cf
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-04-17 17:25:58 +0100

    Update problemaA.ml

--- ProblemaA/problemaA.ml
+++ ProblemaA/problemaA.ml
@@ -5,19 +5,17 @@ ocamlopt -I f_parser/ f_parser.cmxa problemaA.ml -o problemaA.exe
 *)
 type variable = string
 
-type formula =
-  | Var of variable
-  | Not of formula
-  | And of formula * formula
-  | Or of formula * formula
-  | Implies of formula * formula
-  | Equiv of formula * formula
-  | True
-  | False
-
 type nor_formula =
-  | Var of variable
-  | Nor of nor_formula * nor_formula
+  | Var of string
+  | Nor of formula * formula
+
+let rec smallest_variable form smallest_so_far =
+  match form with
+  | Var v -> if v<smallest_so_far then smallest_so_far = v else smallest_so_far = smallest_so_far
+  | Expression(f, g) -> smallest_variable f smallest_so_far; smallest_variable g smallest_so_far
+  (* if ('Z')<(smallest_so_far='Z') *)
+
+let smallest = smallest_variable form 'Z';;
 
 let rec to_nor (f : formula) : nor_formula = 
   match f with
@@ -27,19 +25,13 @@ let rec to_nor (f : formula) : nor_formula =
   | Or (a, b) -> Nor(Nor(to_nor a, to_nor b), Nor(to_nor a, to_nor b))
   | Implies (a, b) -> to_nor (Not (Nor((Not a), b)))
   | Equiv (a, b) -> to_nor (And(Implies(a, b), Implies(b, a)))
-  | False -> Nor(to_nor f, (Nor(to_nor f, to_nor f)))
+  | False -> Nor(Var smallest, (Nor(Var smallest, Var smallest))) (*to_nor And(Not smallest, smallest)*)
   | True -> to_nor (Not(False f))
 
 let rec formula_to_string (f : formula) : string =
   match f with
   | Nor (f1, f2) -> Printf.sprintf "(%s %% %s)" (formula_to_string f1) (formula_to_string f2)
-  | And (f1, f2) -> Printf.sprintf "(%s & %s)" (formula_to_string f1) (formula_to_string f2)
-  | Or (f1, f2) -> Printf.sprintf "(%s | %s)" (formula_to_string f1) (formula_to_string f2)
-  | Not f1 -> Printf.sprintf "!%s" (formula_to_string f1)
-  | Implies (f1, f2) -> Printf.sprintf "(%s -> %s)" (formula_to_string f1) (formula_to_string f2)
-  | Equiv (f1, f2) -> Printf.sprintf "(%s <-> %s)" (formula_to_string f1) (formula_to_string f2)
-  | True f1 -> "TRUE"
-  | False f1 -> "FALSE"
+  | Var f -> Printf.sprintf "(%s)" (string f)
 
 let f_list = parse "stdin"
 
