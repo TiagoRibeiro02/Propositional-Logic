4864cc2d948863494f55c0a5020e2c3c0d53adf0
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2023-04-19 20:16:07 +0100

    Update problemaA.ml

--- ProblemaA/problemaA.ml
+++ ProblemaA/problemaA.ml
@@ -9,9 +9,9 @@ type nor_formula =
   | V of string
   | Nor of nor_formula * nor_formula
 
-let rec smallest_variable form aux =
-  match form with
-  | Var v -> if v<aux then smallest_variable form v else smallest_variable form aux
+let rec smallest_variable expression aux =
+  match expression with
+  | Var v -> if v<aux then smallest_variable expression v else smallest_variable expression aux
   | Not f -> smallest_variable f aux
   | And(f, g) -> let acc = smallest_variable f aux in smallest_variable g acc
   | Or(f, g) -> let acc = smallest_variable f aux in smallest_variable g acc
@@ -34,7 +34,7 @@ let rec to_nor (f : formula_t) (smallest: string) : nor_formula =
 let rec formula_to_string (f : nor_formula) : string =
   match f with
   | Nor (f1, f2) -> Printf.sprintf "(%s %% %s)" (formula_to_string f1) (formula_to_string f2)
-  | V f -> Printf.sprintf "(%s)" (string f)
+  | V f -> Printf.sprintf "(%s)" (f)
 
 let f_list = parse "stdin"
 
@@ -48,3 +48,34 @@ let smallest = smallest_variable form "Z";;
 let () =
   let result = to_nor form smallest in
   formula_to_string result |> print_endline
+
+(*
+Algoritmo de "smallest_variable":
+
+EXEMPLO 1:
+let smallest = smallest_variable AND(B, C) 'Z' =
+let acc = smallest_variable B aux='Z' in smallest_variable C acc
+smallest_variable B aux='Z' = "B"
+smallest_variable C "B"
+= {B}
+logo, AND(B,C) = {B}
+-----------------------------------------------------------------------------
+
+EXEMPLO 2:
+let smallest = smallest_variable And[C, Or(FALSE, D)] 'Z' =
+let acc = smallest_variable C aux='Z' in smallest_variable (Or(FALSE, D)) acc
+smallest_variable C aux='Z' = "C"
+smallest_variable {Or(FALSE, D)} "C"
+smallest_variable {D} "C"
+= C
+
+Calculos auxiliares:
+let smallest = smallest_variable Or(FALSE, D) 'Z' =
+let acc = smallest_variable FALSE aux='Z' in smallest_variable D acc
+smallest_variable FALSE aux='Z' = "Z"
+smallest_variable D "Z"
+= {D}
+logo, Or(FALSE, D) = {D}
+
+
+*)
